0db5a94b0d0ff668064380d71cd25642a741a9c9
DESKTOP-C729OBU\pc1 <satoshi13666@yahoo.co.jp>
2021-09-02 08:43:05 +0900

    learn jsx syntax

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return <input type="text" onClick={() =>{console.log("I am clicked")}} />
+  }
+}
+
+export default App;
