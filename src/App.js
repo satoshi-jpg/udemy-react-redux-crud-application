750be9c98f20b342cbd67000b53cf6d13676daf0
DESKTOP-C729OBU\pc1 <satoshi13666@yahoo.co.jp>
2021-09-02 09:31:56 +0900

    create function components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class App extends Component {
-  render() {
-    return <input type="text" onClick={() =>{console.log("I am clicked")}} />
-  }
+// class App extends Component {
+//   render() {
+//     return <input type="text" onClick={() =>{console.log("I am clicked")}} />
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
